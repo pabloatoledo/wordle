315c9a82407670a2d2eb577ad690713e2f2ee934
pabloatoledo <103152688+pabloatoledo@users.noreply.github.com>
2022-06-27 21:47:00 -0300

    carga inputs con valores en la matriz

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
 window.onload = function() {
+
+    
+
     var matriz = [
         [0, 2, 4, 6, 8],
         [1, 3, 5, 7, 9],
@@ -8,11 +11,15 @@ window.onload = function() {
         [4, 8, 0, 1, 8]
     ]
 
+
     for (let iFila = 0; iFila < matriz.length; iFila++) {
         const element = matriz[iFila];
         for (let iCol = 0; iCol < 5; iCol++) {
             const element = matriz[iCol];
-            console.log(matriz[iFila][iCol])
+            var casilla = "r"+iFila+"c"+iCol
+            var casillero = document.getElementById(casilla)
+            //console.log("casilla: ", casilla, "nro:" ,matriz[iFila][iCol])
+            casillero.value = matriz[iFila][iCol]
         }
     }
 }
