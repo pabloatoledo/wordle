97870a9ba778f8d46731418b3bf35aea2961d668
pabloatoledo <103152688+pabloatoledo@users.noreply.github.com>
2022-07-31 22:07:35 -0300

    funcionan borrar y enter; desactiva los inputs

--- js/app.js
+++ js/app.js
@@ -15,37 +15,65 @@ window.onload = function() {
         ["", "", "", "", ""]
     ]
 
+    //inicio
+
+    desCampos()
+
     //eventos
 
     enviar.addEventListener("click", validaPalabra)
     document.addEventListener("keydown", focus)
-
+    
     //funciones
 
-    function validaPalabra () {
+    function desCampos() {
+        for (let iFila = 0; iFila < letras.length; iFila++) {
+            for (let iCol = 0; iCol < letras[iFila].length; iCol++) {
+                var id = "r"+iFila+"c"+iCol
+                var celda = document.getElementById(id)
+                celda.disabled = true
+            }
+        }
+    }
+
+    function validaPalabra() {
         if(validaRenglonCompleto()) {
             console.log("ok")
+            colActual = 0
         } else {
             console.log("DEBE COMPLETAR EL RENGLÓN!")
         }
     }
 
-    function focus () {
-        if(event.keyCode > 64 && event.keyCode < 91 || event.keyCode == 192) {
+    function focus() {
+        if(colActual < 5 && event.keyCode > 64 && event.keyCode < 91 || event.keyCode == 192) {
             var celdaActual = "r" + filaActual + "c" + colActual
             var celda = document.getElementById(celdaActual)
             celda.value = event.key
-            if(colActual < 4) {
+            letras[filaActual][colActual] = event.key
+            if(colActual < 5) {
                 colActual++
-            } else {
-                validaPalabra()
-                colActual = 0
-                filaActual++
             }
+        }
+        if(event.keyCode == 13) {
+            validaPalabra()
+            if(filaActual < 5) {
+                filaActual++
+            } else {
+                console.log("JUEGO FINALIZADO!")
+            }
+        }
+        if(event.keyCode == 8) {
+            if(colActual > 0) {
+                colActual--
+            }
+            var celdaActual = "r" + filaActual + "c" + colActual
+            var celda = document.getElementById(celdaActual)
+            celda.value = ""
         }
     }
 
-    function validaRenglonCompleto () {
+    function validaRenglonCompleto() {
         var filaCompleta = true
         for (let iiFila = 0; iiFila < 5; iiFila++) {
             var celda = "r"+filaActual+"c"+iiFila
@@ -53,7 +81,6 @@ window.onload = function() {
             
             if (iCelda.value != "") {
                 filaCompleta = filaCompleta * true
-                iCelda.disabled = true
             } else {
                 filaCompleta = filaCompleta * false
             }
